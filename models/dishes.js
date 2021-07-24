0d13606ca142d7a5e2f7fc5b1291fad6f90dc91c
dh711 <dhairya711@gmail.com>
2021-07-24 13:26:13 +0400

    separated comments from dishes object, now its own object ref-ing dishes.

--- models/dishes.js
+++ models/dishes.js
@@ -4,29 +4,6 @@ require("mongoose-currency").loadType(mongoose);
 // Adding a currency type to mongoose.
 const Currency = mongoose.Types.Currency;
 
-const Schema = mongoose.Schema;
-const commentSchema = new Schema(
-    {
-        rating: {
-            type: Number,
-            min: 1,
-            max: 5,
-            required: true,
-        },
-        comment: {
-            type: String,
-            required: true,
-        },
-        author: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        },
-    },
-    {
-        timestamps: true,
-    }
-);
-
 const dishSchema = new Schema(
     {
         name: {
@@ -60,7 +37,6 @@ const dishSchema = new Schema(
             type: String,
             required: true,
         },
-        comments: [commentSchema],
     },
     {
         timestamps: true,
